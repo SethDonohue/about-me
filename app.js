e0a1d74042a57933181f95f9bc853e4c85857e1e
Seth Donohue <donohue.seth@gmail.com>
2017-09-19 16:50:21 -0700

    App.js was commented out... new commit and push to make sure JS runs!

--- app.js
+++ app.js
@@ -4,52 +4,52 @@
 // Use CSS to style your page as you see fit, to the level of abilities in CSS that you currently have
 // Add, Commit, Push process is being followed; it is evident in GitHub that commits are made regularly, and with good commit messages that explain the WHY of the commit.
 
-// var userName = prompt('What is your name?');
-// alert('Hi, ' + userName + ' nice to meet you. See if you can get the right answers to the following questions about me.');
-//
-// //Question 1
-// var answer1 = prompt('Do I have 2 cars?').toLowerCase();
-// console.log('Answer1: ' + answer1);
-//
-// if (answer1 === 'n' || answer1 === 'no') {
-//   //Tell the user they are correct.
-//   alert('You\'re right!');
-// } else {
-//   //Tell the user they are incorrect.
-//   alert('You\'re incorrect, sorry!');
-// }
-//
-// //Question 2
-// var answer2 = prompt('Do I like road trips?').toLowerCase();
-// console.log('Answer2: ' + answer2);
-//
-// if (answer2 === 'y' || answer2 === 'yes') {
-//   //Tell the user they are correct.
-//   alert('You\'re right!');
-// } else {
-//   //Tell the user they are incorrect.
-//   alert('Doh! You\'re incorrect, sorry!');
-// }
-//
-// //Question 3
-// var answer3 = prompt('Do I live in Seattle?').toLowerCase();
-// console.log('Answer3: ' + answer3);
-//
-// if (answer3 === 'y' || answer3 === 'yes') {
-//   //Tell the user they are correct.
-//   alert('You\'re right! That was an easy one... good luck with the next question.');
-// } else {
-//   //Tell the user they are incorrect.
-//   alert('Doh! You\'re wrong! Come on, that was an easy one!');
-// }
-// //Question 4
-// var answer4 = prompt('Do I like working with electronics, welding, blacksmithing, or just creative hobbies in general?').toLowerCase();
-// console.log('Answer4: ' + answer4);
-//
-// if (answer4 === 'y' || answer4 === 'yes') {
-//   //Tell the user they are correct.
-//   alert('You\'re right! That was actually an easy one too.');
-// } else {
-//   //Tell the user they are incorrect.
-//   alert('Doh! You\'re wrong! Come on, that was an easy one too!');
-// }
+var userName = prompt('What is your name?');
+alert('Hi, ' + userName + ' nice to meet you. See if you can get the right answers to the following questions about me.');
+
+//Question 1
+var answer1 = prompt('Do I have 2 cars?').toLowerCase();
+console.log('Answer1: ' + answer1);
+
+if (answer1 === 'n' || answer1 === 'no') {
+  //Tell the user they are correct.
+  alert('You\'re right!');
+} else {
+  //Tell the user they are incorrect.
+  alert('You\'re incorrect, sorry!');
+}
+
+//Question 2
+var answer2 = prompt('Do I like road trips?').toLowerCase();
+console.log('Answer2: ' + answer2);
+
+if (answer2 === 'y' || answer2 === 'yes') {
+  //Tell the user they are correct.
+  alert('You\'re right!');
+} else {
+  //Tell the user they are incorrect.
+  alert('Doh! You\'re incorrect, sorry!');
+}
+
+//Question 3
+var answer3 = prompt('Do I live in Seattle?').toLowerCase();
+console.log('Answer3: ' + answer3);
+
+if (answer3 === 'y' || answer3 === 'yes') {
+  //Tell the user they are correct.
+  alert('You\'re right! That was an easy one... good luck with the next question.');
+} else {
+  //Tell the user they are incorrect.
+  alert('Doh! You\'re wrong! Come on, that was an easy one!');
+}
+//Question 4
+var answer4 = prompt('Do I like working with electronics, welding, blacksmithing, or just creative hobbies in general?').toLowerCase();
+console.log('Answer4: ' + answer4);
+
+if (answer4 === 'y' || answer4 === 'yes') {
+  //Tell the user they are correct.
+  alert('You\'re right! That was actually an easy one too.');
+} else {
+  //Tell the user they are incorrect.
+  alert('Doh! You\'re wrong! Come on, that was an easy one too!');
+}
